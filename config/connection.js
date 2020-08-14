3449061862843c1995fa82995c24c52adc3ce6b4
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-08-14 14:06:58 -0500

    added starter code, began editing DB connection

---
+++ config/connection.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
+
+
+
+// CODE PULLED FROM MODULE
+
+// const Sequelize = require('sequelize');
+
+// require('dotenv').config();
+
+// // create connection to our db
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+//   });
+// }
+
+// module.exports = sequelize;
