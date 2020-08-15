69f6883bd1cdf3ef8e619d348aa7aa35e7af7420
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-08-15 16:34:56 -0500

    finished models; working on relationships; starting routes

--- models/Tag.js
+++ models/Tag.js
@@ -2,11 +2,22 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
   },
   {
     sequelize,
