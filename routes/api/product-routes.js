3490884cc63b689e9c191f090f2062cca45a5019
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-08-16 19:36:26 -0500

    finally fixed the anomalous column AND the broken PUT route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -83,7 +83,6 @@ router.post('/', (req, res) => {
 });
 
 // update product
-// update product
 router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
@@ -98,7 +97,9 @@ router.put("/:id", (req, res) => {
     .then((productTags) => {
       // get list of current tag_ids
       const productTagIds = productTags.map(({ tag_id }) => tag_id);
+      console.log('This is productTagIds: ', productTagIds);
       // create filtered list of new tag_ids
+      console.log('This is the req.body: ', req.body)
       const newProductTags = req.body.tagIds
         .filter((tag_id) => !productTagIds.includes(tag_id))
         .map((tag_id) => {
@@ -116,11 +117,12 @@ router.put("/:id", (req, res) => {
       return Promise.all([
         ProductTag.destroy({ where: { id: productTagsToRemove } }),
         ProductTag.bulkCreate(newProductTags),
+        console.log('Product updated successfully!')
       ]);
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
